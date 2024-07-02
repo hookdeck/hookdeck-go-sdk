54111e28fbd4cb4006e7f5062e0aacba905e82f0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-02 13:26:41 +0000

    SDK regeneration

--- core/client_option.go
+++ core/client_option.go
@@ -43,6 +43,6 @@ func (c *ClientOptions) cloneHeader() http.Header {
 	headers := c.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/hookdeck/hookdeck-go-sdk")
-	headers.Set("X-Fern-SDK-Version", "v0.4.0")
+	headers.Set("X-Fern-SDK-Version", "v0.4.1")
 	return headers
 }
